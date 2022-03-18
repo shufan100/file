dc41c0e6e4a32e755f0e17f2000efb0dabb5f338
shufan100 <1549248097@qq.com>
2022-03-18 14:12:46 +0800

    1

--- TypeScriptDemo/src/index.ts
+++ TypeScriptDemo/src/index.ts
@@ -1,5 +1,21 @@
+import { hi } from './m';
+
 // 入口文件
 function sum(a: number, b: number): number {
   return a * b;
 }
 console.log(sum(51, 11));
+console.log(hi);
+
+const obj: { name: string; age: number } = { name: '小明', age: 33 };
+obj.age = 44;
+console.log(obj);
+
+// console.log(Promise);
+
+class Person {
+  name: string = 'tom';
+  age: number = 18;
+}
+const per = new Person();
+console.log(per.name, per.age);
--- TypeScriptDemo/webpack.config.js
+++ TypeScriptDemo/webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
     // path: './dist'                           // 两者一样
     path: path.resolve(__dirname, 'dist'),      // 指定打包后的文件目录
     filename: 'bundle.js',                      // 打包ts文件后的文件名字
+    environment: {                              // 告诉webpack不使用箭头函数,(正常编译后的文件是箭头函数)
+      arrowFunction: false
+    }
   },
 
   // webpack打包时要使用的模块
@@ -27,9 +30,29 @@ module.exports = {
     // 指定要加载的规则(会有很多种文件，现在配置编译ts的规则)
     rules: [
       {
-        test: /\.ts$/,                    //指规则生效的文件(用正则表达式去匹配所有以ts结尾的文件)
-        use: 'ts-loader',                 //用ts-loader去处理以ts结尾的文件
-        exclude: /node-modules/,         //哪些文件不处理,不编译
+        test: /\.ts$/,             //指规则生效的文件(用正则表达式去匹配所有以ts结尾的文件)
+        use: [                     //(从后往前执行)用ts-loader去处理以ts结尾的文件转成js，再用babel-loader将新版的js转成旧版的js
+          // 配置babel
+          {
+            loader: 'babel-loader',  //指定加载器
+            options: {  //设置babel（因为babel有浏览器兼容问题
+              presets: [  //给babel设置预定义的环境
+                [
+                  '@babel/preset-env',  //指定环境的插件
+                  {     //配置信息
+                    targets: { // 要兼容的浏览器版本
+                      'chrome': '88',
+                      'ie': '11'  //不支持const,箭头函数,没有Promise
+                    },//如果引用了Promise，但ie11没有，就引用corejs里的Promise的方法
+                    'corejs': '3', //指定corejs的版本
+                    'useBuiltIns': 'usage', //使用corejs的方式'usage' 表示按需加载
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'],
+        exclude: /node-modules/,              //哪些文件不处理,不编译
 
       }
     ]
@@ -41,10 +64,15 @@ module.exports = {
     new CleanWebpackPlugin(),
     // build会自动生成html文件
     new HTMLWebpackPlugin({
-      // title: "自定义title",  // 不用src的html模板，自己生成html文件
-      template: "./src/index.html",// 用这个就用src的html模板
+      title: "自定义title",  // 不用src的html模板，自己生成html文件
+      // template: "./src/index.html",// 用这个就用src的html模板
     })
-  ]
+  ],
+
+  // 用来设置引用的模块（哪些文件可以作为模块）
+  resolve: {
+    extensions: ['.ts', '.js'], // 以这两个扩展名结尾的，都可以作为模块来使用
+  }
 
 
 }
